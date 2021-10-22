c7ba49620f6fee06804c7e78ce31556bb677734b
yaofei zheng <zyf26256@gmail.com>
2021-10-23 01:43:18 +0800

    add macro vs const test

--- README.md
+++ README.md
@@ -11,3 +11,18 @@ Example:
 $ make sizeof_data_types
 ```
 
+### atomic_assembly.c
+
+Use:
+```bash
+$ gcc -Wa,-adhln -g atomic_assembly.c > atomic_assembly.s
+```
+to see assembly difference
+
+### macro_vs_const.c
+
+Use:
+```bash
+$ gcc -Wa,-adhln -g macro_vs_const.c > macro_vs_const.s
+```
+to see assembly difference
---
+++ macro_vs_const.c
@@ -0,0 +1,34 @@
+// *************************************************************************
+//
+// Copyright (C) 2019 - 2021  yaofei zheng
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include <stdio.h>
+#define A 2
+#define B 3
+#define RET (A * B)
+const int a = 2;
+const int b = 3;
+int main(int argc, char *argv[])
+{
+    int ret = RET;
+    printf("%d\n", ret);
+    ret = A * B;
+    printf("%d\n", ret);
+    ret = a * b;
+    printf("%d\n", ret);
+    return 0;
+}
