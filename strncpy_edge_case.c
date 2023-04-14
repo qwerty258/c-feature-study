16b2a23118bf80cdfcef9af384dddde81970747b
yaofei zheng <zyf26256@gmail.com>
2023-04-14 16:48:07 +0800

    Update strncpy_edge_case.c

--- strncpy_edge_case.c
+++ strncpy_edge_case.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BUFF_SIZE 5
+#define BUFF_SIZE 2
 
 int main(int argc, char *argv[])
 {
     char dest[BUFF_SIZE] = {0};
-    char *p_str = "1";
-    strncpy(dest, p_str, 1);
+    char *p_str = "12";
+    strncpy(dest, p_str, BUFF_SIZE);
 
     for (size_t i = 0; i < BUFF_SIZE; i++)
     {
         printf("0x%02X\n", dest[i]);
     }
+
+    printf("%s\n", dest);
 
     return 0;
 }
